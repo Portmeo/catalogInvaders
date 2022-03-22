fea0adf7182461a772ddb04fec1aa33cc026c0a1
Alejandro Expósito Gómez <alejandro.expsito.atmira.com>
2022-03-22 17:21:53 +0100

    catalog invaders sngular

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const fs = require('fs');
+
+getDataCSV();
+
+function getDataCSV() {
+    fs.readFile('./puntuaciones.csv', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading file");
+        } else {
+            const usersData = data.split('\n').map(userScore => {
+                let [name, numericaSystem, score] = userScore.split(',');
+                score = decodeScore(numericaSystem, score.replace('\r', ''));
+                return [name, score];
+            }).sort((a, b) => b[1] - a[1]);
+            writeCSV(usersData)
+        }
+    });
+}
+
+function decodeScore(numericaSystem, score) {
+    const calScore = score.split('').reduce((acc, curr) =>  acc + numericaSystem.indexOf(curr).toString(), 0);
+    return parseInt(calScore, numericaSystem.length);
+}
+
+function writeCSV(data) {
+    fs.writeFile('resultado.csv', data.join('\n'), 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing file');
+        } else {
+            console.log('Success');
+        }
+    });
+}
